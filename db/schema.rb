2b410d2e851eaef131f350d9e2c9276d238ab3f5
Bruno B <34753722+bruno-berchielli@users.noreply.github.com>
2020-06-02 20:52:49 -0300

    more user validations and create email index for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_234600) do
+ActiveRecord::Schema.define(version: 2020_06_02_235215) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
