258d9d81cfb1d711234776a20d41af4a6eac28a8
Bruno B <34753722+bruno-berchielli@users.noreply.github.com>
2020-06-02 19:44:06 -0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :cpf, presence: true, uniqueness: true
+  validates_inclusion_of :role, :in => ['admin', 'student', 'professor'], :allow_nil => false
+  validates :password, presence: true, length: {minimum:6, maximum:16}
 end
