7bd8c3cf8fe0288166100c95160257b5286499f4
Bruno B <34753722+bruno-berchielli@users.noreply.github.com>
2020-06-14 18:44:43 -0300

    validates user in classrooms

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,5 +1,6 @@
 class Classroom < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+  validates :user_id, presence: true
   validates :capacity, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100}
 end
